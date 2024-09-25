1e0d44227192f1925992b5d2d56f3f140c0942fa
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-09-25 16:15:03 -0300

    done

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-import User from '/user.js'
+import User, {printName as printUserName, printAge} from '/user.js'
 
 const user = new User('Bob', 11)
 console.log(user)
+printUserName(user)
+printAge(user)
